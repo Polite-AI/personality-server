4b09df09561d4669bf22aa804a06a3cca945387c
rjp44 <rob@ipcortex.co.uk>
2017-10-12 00:37:09 +0100

    Add npm run test target

--- tests/database.test.js
+++ tests/database.test.js
@@ -18,6 +18,10 @@ messages = [{
       text: 'you stink',
       event_id: '1234560xdeadbeef!!:L!K":LK!"":polite.ai',
       user: '@rob:polite.ai'
+    },
+    classy: {
+      wikidetox: '{ something }',
+      polite_ai: '{ something_else }'
     }
   },
   {
@@ -29,7 +33,17 @@ messages = [{
       text: 'you may smell a bit old man',
       event_id: '1234860xdeadbeef!!:L!K":LK!"":polite.ai',
       user: '@rob:polite.ai'
-    }
+    },
+    classy: {
+      wikidetox: '{ something }',
+      polite_ai: '{ something_else }'
+  },
+    appeal : [
+        {user: '@rob:matrix.org', type: 'report', text: 'this user is very abusive'},
+        {user: 'rob@ipcortex.co.uk', type: 'appeal', text: 'I don\'t think this is that bad TBH'},
+        {user: '@rob:matrix.org', type: 'appeal', text: 'this user is very bad \'; DROP DATABASE'},
+        {user: ';ldfkalksghlwkrlsrgioj;klsgjn;oijrspgoirj;aoiergjsdf;l@rob:matrix.org', type: 'appeal', text: 'this user is very abusive'},
+    ]
   },
   {
     room: {
@@ -40,7 +54,18 @@ messages = [{
       text: 'you slionk',
       event_id: '123adfas4560xdeadbeef!!:L!K":LK!"":techub.slack.com',
       user: 'rob@pickering.org'
-    }
+    },
+    classy: {
+      wikidetox: '{ something }',
+      polite_ai: '{ something_else }'
+  },
+  appeal : [
+      {user: '@rob:doeirmatrix.org', type: 'report', text: 'thil/kms;lasf;s user is very abusive'},
+      {user: 'rob@ipcort;lasdkex.co.uk', type: 'appeal', text: 'I don\'t think this is that bad TBH'},
+      {user: '@rob:matrix.org', type: 'appeal', text: 'this user is very ba DROP DATABASE'},
+      {user: ';ldfka;sdflkas;dfasdf;laksd;flkasd;fklsdf;l@rob:matrix.org', type: 'appeal', text: 'this user is very abusive'},
+  ]
+
   },
   {
     room: {
@@ -113,7 +138,9 @@ test('Database tests: inserting and querying', (t) => {
     // 3 Query for existing messages
     () => {
       return messages.reduce((p, message) => {
-        return p.then(() => { return db.messageGet(message.message, message.room)})
+        return p.then(() => {
+            return db.messageGet(message.message, message.room)
+          })
           .then(row => t.assert(row.id > 0, "Got full message "))
           .catch(err => t.fail(err));
       }, Promise.resolve())
@@ -152,23 +179,59 @@ test('Database tests: inserting and querying', (t) => {
         })
     },
 
-    // 5: Delete all the messages
+    // 5: Add classification data
     () => {
       return messages.reduce((p, message) => {
-        return p.then(() => { return db.messageDestroy(message.message) })
-            .then(row => t.assert(message.shouldFail == null, "Deleted record " + message.message.id))
-            .catch(err => t.assert(message.shouldFail != null, "Failed to delete" + err.message))
+        if(message.classy != null)
+          Object.keys(message.classy)
+          .forEach(c => {
+            p = p.then(() => {
+                return db.messageClassify(message.message, c, message.classy[c])
+              })
+              .then(row => t.assert(row.class_id > 0, 'class created'))
+              .catch(err => t.assert(message.classy.shouldFail != null, "Failed to classify" + err.message))
+          })
+        return p;
+      }, Promise.resolve())
+    },
+
+    // 6: Add appeal data
+    () => {
+      return messages.reduce((p, message) => {
+        if(message.appeal != null)
+          message.appeal.forEach(appeal => {
+            p = p.then(() => {
+                return db.messageAppeal(message.message, appeal.type, appeal.text, appeal.user)
+              })
+              .then(row => t.assert(row.appeal_id > 0, 'appeal created'))
+              .catch(err => t.assert(message.classy.shouldFail != null, "Failed to appeal" + err.message))
+          })
+        return p;
+      }, Promise.resolve())
+    },
+
+
+    // 7: Delete all the messages
+    () => {
+      return messages.reduce((p, message) => {
+        return p.then(() => {
+            return db.messageDestroy(message.message)
+          })
+          .then(row => t.assert(message.shouldFail == null, "Deleted record " + message.message.id))
+          .catch(err => t.assert(message.shouldFail != null, "Failed to delete" + err.message))
 
         ;
       }, Promise.resolve())
     },
 
-    // 6: Check they are Gone
+    // 8: Check they are Gone
     () => {
       return messages.reduce((p, message) => {
-        return p.then(() => { return db.messageGet(message.message, message.room)})
-            .then(row => t.fail("Message should be gone " + message.message.id))
-            .catch(err => t.pass("Message gone " + err.message));
+        return p.then(() => {
+            return db.messageGet(message.message, message.room)
+          })
+          .then(row => t.fail("Message should be gone " + message.message.id))
+          .catch(err => t.pass("Message gone " + err.message));
       }, Promise.resolve())
     }
 
